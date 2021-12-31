740afaef3f6d84b81ab41c39b7ecd63a4761b1f2
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2021-12-31 19:53:05 -0300

    Use state monad in ASCIIscreenEdit

--- ASCIIscreen/ASCIIscreenEdit.hs
+++ ASCIIscreen/ASCIIscreenEdit.hs
@@ -8,45 +8,51 @@ module ASCIIscreen.ASCIIscreenEdit (
     putStringIn
 ) where
 
-import Flow ( (.>) )
-
-import Helpers.FunctionHelper ( applyFor )
+import Control.Monad ( forM )
+import Control.Monad.Trans.State ( execState, modify )
 
 import ASCIIscreen.ASCIIscreen
     ( editAt, putAt, ASCIIscreen, Coords, X_coord, Y_coord ) 
     
 editHorizontalLine :: (Char -> Char) -> Y_coord -> X_coord -> X_coord -> ASCIIscreen -> ASCIIscreen
-editHorizontalLine f y x0 x1 = applyFor [x0..x1] (\x -> editAt f (x, y)) 
+editHorizontalLine f y x0 x1 = execState $ do
+    forM [x0..x1] $ \x -> 
+        modify $ editAt f (x, y)
+     
 
 putHorizontalLine :: Char -> Y_coord -> X_coord -> X_coord -> ASCIIscreen -> ASCIIscreen
 putHorizontalLine c = editHorizontalLine $ const c
 
 
 editVerticalLine :: (Char -> Char) -> X_coord -> Y_coord -> Y_coord -> ASCIIscreen -> ASCIIscreen
-editVerticalLine f x y0 y1 = applyFor [y0..y1] (\y -> editAt f (x, y)) 
+editVerticalLine f x y0 y1 = execState $ do
+    forM [y0..y1] $ \y ->
+        modify $ editAt f (x, y)
 
 putVerticalLine ::  Char -> X_coord -> Y_coord -> Y_coord -> ASCIIscreen -> ASCIIscreen
 putVerticalLine c = editVerticalLine $ const c
 
 
 editRectangle :: (Char -> Char) -> Coords -> Coords -> ASCIIscreen -> ASCIIscreen
-editRectangle f (x0, y0) (x1, y1) = applyFor [y0..y1] (\y -> editHorizontalLine f y x0 x1) 
+editRectangle f (x0, y0) (x1, y1) = execState $ do
+    forM [y0..y1] $ \y ->
+        modify $ editHorizontalLine f y x0 x1
 
 putRectangle :: Char -> Coords -> Coords -> ASCIIscreen -> ASCIIscreen
 putRectangle c = editRectangle $ const c
 
 
 editRectangleWithEdges :: (Char -> Char) -> Coords -> Coords -> ASCIIscreen -> ASCIIscreen
-editRectangleWithEdges f (x0, y0) (x1, y1) =
-    putAt corner (x0, y0)
-    .> putHorizontalLine horizontalBorder y0 (x0 + 1) (x1 - 1)
-    .> putAt corner (x1, y0)
-    .> putVerticalLine verticalBorder x0 (y0 + 1) (y1 - 1)
-    .> putAt corner (x0, y1)
-    .> editRectangle f (x0 + 1, y0 + 1) (x1 - 1, y1 - 1)
-    .> putHorizontalLine horizontalBorder y1 (x0 + 1) (x1 - 1)
-    .> putVerticalLine verticalBorder x1 (y0 + 1) (y1 - 1)
-    .> putAt corner (x1, y1)
+editRectangleWithEdges f (x0, y0) (x1, y1) = execState $ do
+    modify $ putAt corner (x0, y0)
+    modify $ putHorizontalLine horizontalBorder y0 (x0 + 1) (x1 - 1)
+    modify $ putAt corner (x1, y0)
+    modify $ putVerticalLine verticalBorder x0 (y0 + 1) (y1 - 1)
+    modify $ putAt corner (x0, y1)
+    modify $ editRectangle f (x0 + 1, y0 + 1) (x1 - 1, y1 - 1)
+    modify $ putHorizontalLine horizontalBorder y1 (x0 + 1) (x1 - 1)
+    modify $ putVerticalLine verticalBorder x1 (y0 + 1) (y1 - 1)
+    modify $ putAt corner (x1, y1)
     where
         verticalBorder = '|'
         horizontalBorder = '-'
@@ -57,19 +63,16 @@ putRectangleWithEdges c = editRectangleWithEdges $ const c
 
 
 
-
 putStringIn :: String -> Coords -> ASCIIscreen -> ASCIIscreen
-putStringIn ss (x, y) =
-    applyFor lines_ss (\(i, cs) ->
-        putLineIn cs (x, y + i)
-    )
+putStringIn ss (x, y) = execState $ do
+    forM lines_ss $ \(i, cs) ->
+        modify $ putLineIn cs (x, y + i)
     where
         lines_ss = zip [0,1..] $ lines ss
         putLineIn :: String -> Coords -> ASCIIscreen -> ASCIIscreen -- puts xs assuming that it has no '\n'
-        putLineIn cs (x, y) =
-            applyFor chars_cs (\(j, c) ->
-                putAt c (x + j, y)
-            )
+        putLineIn cs (x, y) = execState $ do
+            forM chars_cs $ \(j, c) ->
+                modify $ putAt c (x + j, y)
             where
                 chars_cs = zip [0,1..] cs
 
